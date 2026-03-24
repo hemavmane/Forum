5132ce65a9caa636963d30d01e8b6105c3a4f130
Hema <hema@Hemas-MacBook-Air.local>
2026-03-24 23:14:20 +0530

    created new branch to maintain backend and frontend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,7 +3,7 @@ import {VITE_API_URL} from "../services/constant"
 const API = axios.create({
   baseURL: VITE_API_URL
 });
-
+console.log(VITE_API_URL)
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
   if (token) {
